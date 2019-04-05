148b4d5098c92a0e4e5fd61e541645824e559939
Javier Casaudoumecq <javier@casaudoumecq.me>
2019-04-05 11:12:32 -0400

    Bounce animations and fixes

--- src/main/java/com/applicaster/onboarding/screen/presentation/onboarding/adapters/SegmentRecyclerViewAdapter.kt
+++ src/main/java/com/applicaster/onboarding/screen/presentation/onboarding/adapters/SegmentRecyclerViewAdapter.kt
@@ -2,10 +2,12 @@ package com.applicaster.onboarding.screen.presentation.onboarding.adapters
 
 
 import android.app.Activity
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.Toast
 import com.applicaster.onboarding.screen.model.Segment
@@ -13,6 +15,9 @@ import com.applicaster.onboarding.screen.presentation.onboarding.OnListFragmentI
 import com.applicaster.onboardingscreen.R
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_segment.view.*
+import android.support.v17.leanback.transition.TransitionHelper.setInterpolator
+
+
 
 class SegmentRecyclerViewAdapter(
         private val mValues: List<Segment>,
@@ -35,7 +40,7 @@ class SegmentRecyclerViewAdapter(
 
         Glide.with(activity).load(item.imageUrl).into(holder.segmentImageView)
 
-        holder.mView.setOnClickListener {
+        holder.segmentCardview.setOnClickListener {
             if (holder.selectIcon.isSelected) {
                 holder.selectIcon.isSelected = false
                 holder.selectIcon.setImageDrawable(activity.resources.getDrawable(R.drawable.ic_favorite_unselected))
@@ -43,6 +48,10 @@ class SegmentRecyclerViewAdapter(
                 holder.selectIcon.isSelected = true
                 holder.selectIcon.setImageDrawable(activity.resources.getDrawable(R.drawable.ic_favorite_selected))
             }
+            val bounce = AnimationUtils.loadAnimation(activity, R.anim.bounce)
+            val interpolator = MyBounceInterpolator(0.2, 20.00)
+            bounce.interpolator = interpolator
+            holder.segmentCardview.startAnimation(bounce)
             Toast.makeText(activity,"Subscribed to " + item.title.en, Toast.LENGTH_LONG).show()
             mListener?.onListFragmentInteraction(item as Any)
         }
@@ -53,5 +62,21 @@ class SegmentRecyclerViewAdapter(
     inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         val segmentImageView: ImageView = mView.segment_imageview
         val selectIcon: ImageView = mView.selection_icon_imageview
+        val segmentCardview: CardView = mView.segement_cardview
+    }
+
+    internal inner class MyBounceInterpolator(amplitude: Double, frequency: Double) : android.view.animation.Interpolator {
+        private var mAmplitude = 1.0
+        private var mFrequency = 10.0
+
+        init {
+            mAmplitude = amplitude
+            mFrequency = frequency
+        }
+
+        override fun getInterpolation(time: Float): Float {
+            return (-1.0 * Math.pow(Math.E, -time / mAmplitude) *
+                    Math.cos(mFrequency * time) + 1).toFloat()
+        }
     }
 }
