050f04abc6ac367a9f50f0fd84ef5b476246df7b
Dmytro Puzak <dmytro.puzak@corewillsoft.com>
2020-01-31 18:24:13 +0200

    Resolve crash issue on setting not all plugin parameters. Added default values.

--- src/main/java/com/applicaster/onboarding/screen/mapper/PluginDataMapper.java
+++ src/main/java/com/applicaster/onboarding/screen/mapper/PluginDataMapper.java
@@ -9,12 +9,17 @@ import java.util.Map;
 public class PluginDataMapper {
 
     public PluginConfig mapParamsToConfig(Map params) {
-        String onBoardingFeedPath = (String) params.get("onBoardingFeedPath");
-        String backgroundColor = (String) params.get("backgroundColor");
-        String highlightColor = (String) params.get("highlightColor");
-        String titleColor = (String) params.get("titleColor");
-        String categoryBackgroundColor = (String) params.get("categoryBackgroundColor");
-        boolean applyBorder = StringUtil.booleanValue(params.get("applyBorder").toString());
+        String onBoardingFeedPath = getParam(params, "onBoardingFeedPath", "");
+        String backgroundColor = getParam(params, "backgroundColor", "#000000");
+        String highlightColor = getParam(params, "highlightColor", "#DD4400");
+        String titleColor = getParam(params, "titleColor", "#FFFFFF");
+        String categoryBackgroundColor = getParam(params, "categoryBackgroundColor", "#000000");
+        boolean applyBorder = StringUtil.booleanValue(getParam(params, "applyBorder", "false"));
         return new PluginConfig(onBoardingFeedPath, backgroundColor, highlightColor, titleColor, categoryBackgroundColor, applyBorder);
+    }
+
+    private <T> T getParam(Map params, String key, T defaultValue) {
+        Object param = params.get(key);
+        return param == null ? defaultValue : (T) param;
     }
 }
